c928adb3e85918f92e15a2c21eb525533b098cc4
Dmitry <a1@macbook-air-1.local>
2023-09-24 22:17:48 +0300

    feat: added worked code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
+    
+
+    atStart = 0;
+
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.atStart = Math.ceil(this.min/2 + this.max/2);
+        return this.atStart;
     }
 
     lower() {
-
+        this.max = this.atStart;
     }
 
     greater() {
-
+        this.min = this.atStart;
     }
 }
 
